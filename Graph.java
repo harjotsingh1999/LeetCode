f7ff0c5f897c48edf13f500df2f7d19bdf27c49f
Harjot Singh <asingh.harjot@gmail.com>
2021-02-22 14:39:38 +0530

    22-02-2021 #1

--- Graph.java
+++ Graph.java
@@ -1,21 +1,35 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Graph {
 
-    // adjency matrix is a list containing lists of all connected nodes of all nodes
-    // of the graph
-    public static void addEdge(ArrayList<ArrayList<Integer>> adjMatrix, int u, int v) {
-        adjMatrix.get(u).add(v);
-        adjMatrix.get(v).add(u);
+    int nVertices;
+    List<List<Integer>> adjencyMatrix;
+
+    Graph(int n) {
+        nVertices = n;
+        adjencyMatrix = new ArrayList<List<Integer>>(nVertices);
+        for (int i = 0; i < nVertices; i++) {
+            adjencyMatrix.add(new ArrayList<Integer>());
+        }
     }
 
-    public static void printGraph(ArrayList<ArrayList<Integer>> adjMatrix) {
-        for (int i = 0; i < adjMatrix.size(); i++) {
+    // adjency matrix is a list containing lists of all connected nodes of all nodes
+    // of the graph
+    public void addEdge(int u, int v) {
+        adjencyMatrix.get(u).add(v);
+        adjencyMatrix.get(v).add(u);
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < adjencyMatrix.size(); i++) {
             System.out.println("Adjency List of vertex " + i);
-            for (int j = 0; j < adjMatrix.get(i).size(); j++) {
-                System.out.print("-> " + adjMatrix.get(i).get(j));
+            for (int j = 0; j < adjencyMatrix.get(i).size(); j++) {
+                System.out.print("-> " + adjencyMatrix.get(i).get(j));
             }
             System.out.println();
         }
@@ -25,19 +39,19 @@ public class Graph {
     // graph starting from 0.
 
     // if graph started from 1 the visited array would be of size nVertices+1
-    public static void BFS(ArrayList<ArrayList<Integer>> adjMatrix, int nVertices) {
+    public void BFS(int start) {
         boolean[] visited = new boolean[nVertices];
         for (int i = 0; i < visited.length; i++) {
             visited[i] = false;
         }
         Queue<Integer> queue = new LinkedList<Integer>();
-        visited[0] = true;
-        queue.add(0);
+        visited[start] = true;
+        queue.add(start);
         while (!queue.isEmpty()) {
             int current = queue.poll();
             System.out.print(current + " ");
-            for (int i = 0; i < adjMatrix.get(current).size(); i++) {
-                int vertex = adjMatrix.get(current).get(i);
+            for (int i = 0; i < adjencyMatrix.get(current).size(); i++) {
+                int vertex = adjencyMatrix.get(current).get(i);
                 if (visited[vertex] == false) {
                     visited[vertex] = true;
                     queue.add(vertex);
@@ -46,39 +60,145 @@ public class Graph {
         }
     }
 
-    public void DFS(int startingVertex, int numberofVertices, ArrayList<ArrayList<Integer>> adjMatrix) {
-        boolean[] visited = new boolean[numberofVertices];
-        DFSUtil(startingVertex, visited, adjMatrix);
+    public void DFS(int startingVertex) {
+        boolean[] visited = new boolean[nVertices];
+        DFSUtil(startingVertex, visited);
     }
 
-    public void DFSUtil(int vertex, boolean[] visited, ArrayList<ArrayList<Integer>> adjMatrix) {
+    public void DFSUtil(int vertex, boolean[] visited) {
 
         // mark this vertex as visited
         visited[vertex] = true;
 
         // print it
-        System.out.println(vertex + ", ");
+        System.out.print(vertex + " ");
 
         // then for all its adjascent vertices
         // perform the same action if they are not already visited
-        for (int v : adjMatrix.get(vertex)) {
+        for (int v : adjencyMatrix.get(vertex)) {
             if (!visited[v])
-                DFSUtil(v, visited, adjMatrix);
+                DFSUtil(v, visited);
         }
     }
 
-    public static void main(String[] args) {
-        int nVertices = 5;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+    // number of connected components is the number of interconnected graphs
+    // that are disconnected from the others
+    public void countNumberOfConnectedComponents() {
+        int count = 0;
+        int[] connectedWith = new int[nVertices];
+        boolean[] visited = new boolean[nVertices];
         for (int i = 0; i < nVertices; i++) {
-            adj.add(new ArrayList<Integer>());
+            if (!visited[i]) {
+                count += 1;
+                DFSUtilForConnectedComponents(i, visited, count, connectedWith);
+            }
         }
+        System.out.println("No of connected components= " + count);
+        System.out.println("connected with Array= " + Arrays.toString(connectedWith));
+    }
 
-        addEdge(adj, 0, 1);
-        addEdge(adj, 0, 2);
-        addEdge(adj, 0, 3);
-        addEdge(adj, 2, 4);
-        printGraph(adj);
-        BFS(adj, nVertices);
+    public void DFSUtilForConnectedComponents(int vertex, boolean[] visited, int count, int[] connectedWith) {
+
+        // mark this vertex as visited
+        visited[vertex] = true;
+
+        // print it
+        connectedWith[vertex] = count;
+
+        // then for all its adjascent vertices
+        // perform the same action if they are not already visited
+        for (int v : adjencyMatrix.get(vertex)) {
+            if (!visited[v])
+                DFSUtilForConnectedComponents(v, visited, count, connectedWith);
+        }
+    }
+
+    // 0 <= start, end < nVertices
+    public void printShortestPath(int start, int end) {
+        int[] parentOfNodes = BFSForPath(start);
+        // the abpve array contains the parent vertex of each element
+        // means parentOfNodes[i] will be the parent of i
+        // from where it was reached
+        reconstructPath(start, end, parentOfNodes);
+    }
+
+    private void reconstructPath(int start, int end, int[] parentOfNodes) {
+
+        // starting from the end node
+        // going to the parent of the node at each iteration
+        // till we reach the end (or -1)
+        List<Integer> verticesInPath = new ArrayList<>();
+        for (int i = end; i != -1; i = parentOfNodes[i]) {
+            verticesInPath.add(i);
+        }
+        // since we are adding from the end
+        // we need to reverse
+        Collections.reverse(verticesInPath);
+
+        // check if "end" is actually reachable from "start"
+        if (verticesInPath.get(0) == start) {
+            System.out.println("Path from " + start + " to " + end);
+            for (Integer integer : verticesInPath) {
+                System.out.print(integer + "->");
+            }
+        } else {
+            System.out.println(end + " is not reachable from " + start);
+        }
+    }
+
+    public int[] BFSForPath(int start) {
+        boolean[] visited = new boolean[nVertices];
+        for (int i = 0; i < visited.length; i++) {
+            visited[i] = false;
+        }
+        int[] parentOfNodes = new int[nVertices];
+        Arrays.fill(parentOfNodes, -1);
+        Queue<Integer> queue = new LinkedList<Integer>();
+        visited[start] = true;
+        queue.add(start);
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.print(current + " ");
+            for (int i = 0; i < adjencyMatrix.get(current).size(); i++) {
+                int vertex = adjencyMatrix.get(current).get(i);
+                if (visited[vertex] == false) {
+                    visited[vertex] = true;
+                    queue.add(vertex);
+                    parentOfNodes[vertex] = current;
+                }
+            }
+        }
+        System.out.println();
+        return parentOfNodes;
+    }
+
+    public static void main(String[] args) {
+        int nVertices = 18;
+        Graph graph = new Graph(nVertices);
+        graph.addEdge(0, 8);
+        graph.addEdge(0, 13);
+        graph.addEdge(0, 4);
+        graph.addEdge(0, 14);
+        graph.addEdge(8, 4);
+        graph.addEdge(8, 14);
+        graph.addEdge(13, 14);
+        graph.addEdge(1, 5);
+        graph.addEdge(5, 16);
+        graph.addEdge(5, 17);
+        graph.addEdge(3, 9);
+        graph.addEdge(9, 15);
+        graph.addEdge(9, 2);
+        graph.addEdge(15, 2);
+        graph.addEdge(10, 15);
+        graph.addEdge(7, 6);
+        graph.addEdge(6, 11);
+        graph.addEdge(11, 7);
+        // graph.printGraph();
+        // graph.BFS(1);
+        // System.out.println();
+        // graph.DFS(1);
+        // System.out.println();
+        // graph.countNumberOfConnectedComponents();
+        graph.printShortestPath(12, 4);
     }
 }
